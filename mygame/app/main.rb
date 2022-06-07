753813b5b0d9853fd0ab2b7b24c87b63e306914a
Kehvarl <Kehvarl@Kehvarl.com>
2022-06-06 19:30:16 -0700

    Part-1 - Drawing the Background

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,6 +1,3 @@
 def tick args
-  args.outputs.labels  << [640, 500, 'Hello DragonRuby!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! http://discord.dragonruby.org', 5, 1]
-  args.outputs.sprites << [576, 280, 128, 101, 'dragonruby.png']
+  args.outputs.solids  << [0, 0, 1280, 720, 0, 0, 0]
 end
