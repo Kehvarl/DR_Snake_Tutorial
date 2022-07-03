c3894f82509dd40438886abcf55e41d8ae6eb89d
Kehvarl <Kehvarl@Kehvarl.com>
2022-07-03 15:53:36 -0700

    Part-4 - Collisions - fix broken collisions.

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -13,6 +13,14 @@ def make_wall_coords
   wall_coords
 end
 
+def make_obstacles
+  obstacles = []
+  (32..96). each do |x|
+    obstacles << [x,24]
+  end
+  obstacles
+end
+
 def draw_array (arr, color)
   out = []
   arr.each do |e|
@@ -23,8 +31,10 @@ end
 
 def initialize args
   args.state.update ||=1
-  args.state.walls_coords = make_wall_coords
-  args.state.walls = draw_array(args.state.walls_coords, {r:255, g:0, b:0})
+  args.state.walls_coords ||= make_wall_coords
+  args.state.obstacle_coords ||= make_obstacles
+  args.state.walls ||= draw_array(args.state.walls_coords, {r:255, g:0, b:0})
+  args.state.obstacles ||= draw_array(args.state.obstacle_coords, {r: 128, g: 0, b: 128})
   args.state.snake.x ||= 64
   args.state.snake.y ||= 36
   args.state.snake.vx ||= 1
@@ -36,9 +46,7 @@ def update_snake args
   if args.state.update <= 0
     args.state.snake.x += args.state.snake.vx
     args.state.snake.y += args.state.snake.vy
-    if args.state.snake.x > 128 || args.state.snake.x < 0
-      args.state.snake.vx = -args.state.snake.vx
-    end
+
     args.state.update = 3
   end
 end
@@ -49,6 +57,7 @@ def render args
   args.outputs.solids  << {x: args.state.snake.x*10, y:args.state.snake.y*10, w:10, h:10, r:0, g:128, b:0}
 
   args.outputs.solids << args.state.walls
+  args.outputs.solids << args.state.obstacles
 end
 
 def tick args
@@ -71,10 +80,9 @@ def tick args
   end
 
   update_snake args
-  args.state.walls.each do |w|
-    if args.state.wall_coords.include? [args.state.snake.x, args.state.snake.y]
-      puts "Collided with wall!"
-    end
+  if args.state.wall_coords.include? [args.state.snake.x, args.state.snake.y]
+    args.state.snake.vx = -args.state.snake.vx
+    args.state.snake.x += 2*args.state.snake.vx
   end
   render args
 end
